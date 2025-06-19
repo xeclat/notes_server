25d5c3afc435acfde6414f97e5c74e54c5db0eb5
xeclat <128238945+xeclat@users.noreply.github.com>
2025-06-19 06:25:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -26,11 +26,6 @@ let notes = [
     important: true
   }
 ]
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
-
 app.get('/api/notes', (request, response) => {
     response.json(notes)
 })
