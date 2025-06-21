e595b9496bab20fbbbf942f12995573f06a817a9
xeclat <akwisame@gmail.com>
2025-06-21 12:46:34 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express')
-const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
 app.use(express.static('dist'))
-app.use(cors())
 app.use(morgan('tiny'))
 
 
