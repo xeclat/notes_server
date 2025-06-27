895e1222e8cee2629e659391e7422eb775bcc600
xeclat <akwisame@gmail.com>
2025-06-27 16:00:56 +0200

    db and validation support

--- index.js
+++ index.js
@@ -1,78 +1,105 @@
+require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
+const Note = require('./models/note')
+
 const app = express()
 
-app.use(express.json())
-app.use(express.static('dist'))
-app.use(morgan('tiny'))
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message})
   }
-]
+
+  next(error)
+}
+
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(requestLogger)
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
 
 app.get('/api/notes', (request, response) => {
+  Note.find({}).then((notes) => {
     response.json(notes)
+  })
 })
 
-app.get('/api/notes/:id', (request, response) =>{
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-
-    if(note) {
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
         response.json(note)
-    } else {
+      } else {
         response.status(404).end()
-    }
+      }
+    })
+    .catch((error) => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
 
-    response.status(204).end()
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
+
+  note.save().
+    then((savedNote) => {
+     response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
-const generatedId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(... notes.map(n => Number(n.id)))
-        : 0
-    return String(maxId + 1)
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
+
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content
+      note.important = important
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
+    })
+    .catch((error) => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
 }
-app.post('/api/notes', (request, response) => {
-    const body = request.body
-    
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
 
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        id: generatedId()
-    }
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
-    notes = notes.concat(note)
-
-    response.json(note)
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
